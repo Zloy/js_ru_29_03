79a0986f19ff28dd031a3b53435bb764fb478bcf
Roman <kaboom.ua@gmail.com>
2016-04-07 22:48:26 +0300

    Update CommentStore.js

--- src/stores/CommentStore.js
+++ src/stores/CommentStore.js
@@ -14,6 +14,7 @@ class CommentStore extends SimpleStore {
                 case ADD_COMMENT:
                     var newItem = { id: this.getNewItemId(), text: text };
                     this.__add(newItem);
+                    //нет, стор должен менять только сам себя, для этого мы и сделали __add приватным
                     stores.articles.addComment(articleId, newItem.id);
                     this.emitChange();
                     break;
